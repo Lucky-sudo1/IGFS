7e841c721b48e6dedcd07cc8978c4fdc47dc5d90
Lucky-sudo1 <3352609084@qq.com>
2025-12-04 09:27:41 +0800

    Refactor probability calculation and add new logic

--- onemohulei.m
+++ onemohulei.m
@@ -1,11 +1,17 @@
+x3=[0.1,0.4,0.5];
+y3=[0.2,0.2,0.6];
+M3=(x3./(sum(x3)));
+N3=(y3./(sum(y3)));
+d3=(0.5.*(M3(1,1).*log2(((2.*M3(1,1))./(M3(1,1)+N3(1,1))))+M3(1,2).*log2(((2.*M3(1,2))./(M3(1,2)+N3(1,2))))+M3(1,3).*log2(((2.*M3(1,3))./(M3(1,3)+N3(1,3))))+N3(1,1).*log2(((2.*N3(1,1))./(M3(1,1)+N3(1,1))))+N3(1,2).*log2(((2.*N3(1,2))./(M3(1,2)+N3(1,2))))+N3(1,3).*log2(((2.*N3(1,3))./(M3(1,3)+N3(1,3))))))^0.5
+   
 
-% 直接用 relation_matrix 就代表 fuzzy_classes，关系矩阵是 m×n 的普通数组
-fuzzy_classes = relation_matrix;   % 366 x 366 数组，不用 cell
+
+fuzzy_classes = relation_matrix;  
 
 Ci = mean(V_norm, 2); 
 Ci_sum_row = reshape(Ci', 1, []);   % 1 x 366 行向量
 
-% denominator: 每行求和
+%  每行求和
 denominator = sum(fuzzy_classes, 2);    
 
 % numerator: 每行与 C_sum_row 按元素乘积后求和
@@ -18,5 +24,6 @@ Pro(nonzero_idx) = numerator(nonzero_idx) ./ denominator(nonzero_idx);
 
 disp('计算得到的Pro向量为：');
 disp(Pro);
+
 
 
